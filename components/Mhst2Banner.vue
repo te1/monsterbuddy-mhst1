2bca44bb3ec4d7ccdcab827d0c625c8f7eef532f
te <te1@users.noreply.github.com>
2024-06-25 01:42:26 +0200

    fix MHST2 link

--- components/Mhst2Banner.vue
+++ components/Mhst2Banner.vue
@@ -1,28 +1,26 @@
 <template>
-  <client-only>
-    <div
-      v-show="show || force"
-      class="note flex gap-3 bg-[#fef9c3] dark:bg-[#eab308]/25 px-4 py-3"
-    >
-      <div class="flex-1">
-        Looking for info on
-        <strong>Monster Hunter Stories 2: Wings of Ruin</strong> instead? Check
-        out
-        <a class="link" href="https://monsterbuddy.app" target="_blank">
-          Monster Buddy for MHST2</a
-        >.
-      </div>
-
-      <div v-if="!force" class="-mt-3 -mr-3">
-        <AppIconButton
-          :icon="['fas', 'times']"
-          title="Don't show this anymore"
-          class="text-xl"
-          @click="hide"
-        />
-      </div>
+  <div
+    v-show="show || force"
+    class="note flex gap-3 bg-[#fef9c3] dark:bg-[#eab308]/25 px-4 py-3"
+  >
+    <div class="flex-1">
+      Looking for info on
+      <strong>Monster Hunter Stories 2: Wings of Ruin</strong> instead? Check
+      out
+      <a class="link" href="https://monsterbuddy.app" target="_blank">
+        Monster Buddy for MHST2</a
+      >.
     </div>
-  </client-only>
+
+    <div v-if="!force" class="-mt-3 -mr-3">
+      <AppIconButton
+        :icon="['fas', 'times']"
+        title="Don't show this anymore"
+        class="text-xl"
+        @click="hide"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/eggs.vue
+++ pages/eggs.vue
@@ -36,7 +36,9 @@
     <NuxtChild v-show="!leaving && showFilter" />
 
     <main v-show="leaving || !showFilter">
-      <Mhst2Banner class="mb-3" />
+      <client-only>
+        <Mhst2Banner class="mb-3" />
+      </client-only>
 
       <div
         v-if="showActiveFilters"
